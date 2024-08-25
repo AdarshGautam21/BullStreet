a6c2936cf184bcec5eb291a4e03021546a3bacb8
shreyaannnnn <shreyanbharadwaj@gmail.com>
2024-08-25 09:08:45 +0530

    Updated contract

--- utils/config.tsx
+++ utils/config.tsx
@@ -3,7 +3,8 @@ import AssetABI from '@/public/abi/Asset.json'
 // import { ethers } from 'ethers';
 
 export const contracts = {
-    AssetMarket: '0x62232ef913d62ad18107af9bcf3c76179ec7ef64'
+    // AssetMarket: '0x62232ef913d62ad18107af9bcf3c76179ec7ef64'
+    AssetMarket: '0x8c5a818cec781e56f6db2c15bd46c8646f15e75e'
 }
 
 export const abi = {
